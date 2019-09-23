5521624749dd6ce9de5e100711be05e594f61f31
tanya290498 <29tatyana04@gmail.com>
2019-09-23 17:41:53 +0300

    some modules have been imported

--- src/app/account/account.module.ts
+++ src/app/account/account.module.ts
@@ -1,13 +1,22 @@
 import {NgModule} from '@angular/core';
 import {AccountRouting} from './account-routing';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {OverviewComponent} from './overview/overview.component';
+import {SharedModule} from '../shared/shared.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   imports: [
     AccountRouting,
-
+    SharedModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [],
-  declarations: [],
+  declarations: [
+    OverviewComponent,
+    SignInComponent
+  ],
   providers: [],
 })
 export class AccountModule {
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
 import {AuthMasterComponent} from './account/auth-master/auth-master.component';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
 
 const routes: Routes = [
   {
