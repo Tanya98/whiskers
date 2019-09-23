da13b264b10f1a8f052665a9c0107a1b5f13cd7e
tanya290498 <29tatyana04@gmail.com>
2019-09-23 17:37:16 +0300

    input string has been added

---
+++ src/app/shared/components/inputs/input-string/input-string.component.ts
@@ -0,0 +1,37 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {FormControl, FormControlName} from '@angular/forms';
+
+@Component({
+  selector: 'app-input-string',
+  templateUrl: './input-string.component.html',
+  styleUrls: ['./input-string.component.scss']
+})
+
+export class InputStringComponent implements OnInit {
+  constructor() {
+  }
+
+  @Input()
+  public placeholder: string;
+
+  @Input()
+  public name: string;
+
+  @Input()
+  public minLength: number;
+
+  @Input()
+  public maxLength: number;
+
+  @Input()
+  public isRequired: boolean;
+
+  @Input()
+  public type: string;
+
+  @Input()
+  public formControlName: FormControlName;
+
+  ngOnInit() {
+  }
+}
