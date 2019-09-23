926f23786cfa20cfdbcbd2f0c5d86a7a0b97aa74
tanya290498 <29tatyana04@gmail.com>
2019-09-23 17:42:18 +0300

    sign in component has been created

--- src/app/account/sign-in/sign-in.component.ts
+++ src/app/account/sign-in/sign-in.component.ts
@@ -1,13 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LocalizationModel} from '../../shared/resources/localization.model';
+import {ACCOUNT_FIELDS} from '../account-fields.configuration';
+import {LocalizationService} from '../../shared/services/localization.service';
+import {FormBuilder, FormControl, FormControlName, Validators} from '@angular/forms';
 
 @Component({
-    selector: 'app-sign-in',
-    templateUrl: './sing-in.component.html',
-    styleUrls: ['./sign-in.component.scss']
+  selector: 'app-sign-in',
+  templateUrl: './sing-in.component.html',
+  styleUrls: ['./sign-in.component.scss']
 })
 
 export class SignInComponent implements OnInit {
-    constructor() { }
+  constructor(
+    public localizationService: LocalizationService,
+    public  formBuilder: FormBuilder
+  ) {
+    this.localization = this.localizationService.localize;
+  }
 
-    ngOnInit() { }
+  public localization: LocalizationModel;
+
+  public configuration = ACCOUNT_FIELDS;
+
+  public signInForm;
+
+  ngOnInit() {
+    this.signInForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(5)]],
+      password: ['', [Validators.required, Validators.maxLength(8)]]
+    });
+  }
 }
