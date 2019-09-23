5d0d7c58e36f584bc5b65f1de10adf29632d6629
tanya290498 <29tatyana04@gmail.com>
2019-09-23 12:21:01 +0300

    account module has been created

---
+++ src/app/account/account.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {AccountRouting} from './account-routing';
+
+@NgModule({
+  imports: [
+    AccountRouting,
+
+  ],
+  exports: [],
+  declarations: [],
+  providers: [],
+})
+export class AccountModule {
+}
